3270013bf72b4136c14ca8af991bab6cd4bb48be
kayrus <kay.diam@gmail.com>
2016-10-17 16:21:23 +0200

    Implemented curl exit workaround, strip DELETED k8s events

--- docker/kubernetes-events-printer/events.sh
+++ docker/kubernetes-events-printer/events.sh
@@ -9,9 +9,14 @@ KUBERNETES_SERVICE_PORT_HTTPS=${KUBERNETES_SERVICE_PORT_HTTPS:?Please specify KU
 BASE_URL="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS}/api/v1"
 CA_CERT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-RESOURCE_VERSION=$(curl -s "${BASE_URL}/events" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | jq -r '.metadata.resourceVersion')
 
 trap 'kill -TERM $PID' TERM INT
-curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" &
-PID=$!
-wait $PID
+while true; do
+  # curl exits from time to time, most probably because of kube-proxy reloads iptables rules, needs deeper investigation. As for now, let's use a loop trick.
+  RESOURCE_VERSION=$(curl -s "${BASE_URL}/events" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" | jq -r '.metadata.resourceVersion')
+  DATE=$(date --utc +"%Y-%m-%dT%TZ")
+  echo "{\"time\":\"${DATE}\",\"object\":{\"message\":\"Monitoring Kubernetes events staring from ${RESOURCE_VERSION} resourceVersion\"}}" >&2
+  curl -s "${BASE_URL}/watch/events?resourceVersion=${RESOURCE_VERSION}" --cacert "${CA_CERT}" -H "Authorization: Bearer ${TOKEN}" &
+  PID=$!
+  wait $PID
+done
