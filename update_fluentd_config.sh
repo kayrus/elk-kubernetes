ccf9a594a79dba3fe8a3267dfd7cb5fd6821e436
kayrus <kay.diam@gmail.com>
2016-10-20 23:47:27 +0200

    Introduced podAntiAffinity

--- update_fluentd_config.sh
+++ update_fluentd_config.sh
@@ -8,4 +8,4 @@ KUBECTL="kubectl ${KUBECTL_PARAMS} --namespace=\"${NAMESPACE}\""
 
 eval "${KUBECTL} create configmap fluentd-config --from-file=docker/fluentd/td-agent.conf --dry-run -o yaml" | eval "${KUBECTL} apply -f -"
 # Just remove pods and daemonsets will recreate new ones with updated config file
-eval "${KUBECTL} delete $(eval "${KUBECTL} get pods -o name" | awk '/pod\/fluentd-elasticsearch/')"
+eval "${KUBECTL} delete pods -l k8s-app=fluentd-logging,name=fluentd-logging"
