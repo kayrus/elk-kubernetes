750963a6157016eb5b1cad78295c2eac723dde2a
kayrus <kay.diam@gmail.com>
2017-04-27 10:25:23 +0200

    Use kubernetes.io/role="master" condition for apply_labels_on_nodes.sh

--- es5/apply_labels_on_nodes.sh
+++ es5/apply_labels_on_nodes.sh
@@ -15,7 +15,7 @@ print_green() {
 NAMESPACE=${NAMESPACE:-es5}
 KUBECTL="kubectl ${KUBECTL_PARAMS} --namespace=\"${NAMESPACE}\""
 
-NODES=$(eval "${KUBECTL} get nodes -o go-template=\"{{range .items}}{{\\\$name := .metadata.name}}{{\\\$unschedulable := .spec.unschedulable}}{{range .status.conditions}}{{if eq .reason \\\"KubeletReady\\\"}}{{if eq .status \\\"True\\\"}}{{if not \\\$unschedulable}}{{\\\$name}}{{\\\"\\\\n\\\"}}{{end}}{{end}}{{end}}{{end}}{{end}}\"")
+NODES=$(eval "${KUBECTL} get nodes -l 'kubernetes.io/role!=master' -o go-template=\"{{range .items}}{{\\\$name := .metadata.name}}{{\\\$unschedulable := .spec.unschedulable}}{{range .status.conditions}}{{if eq .reason \\\"KubeletReady\\\"}}{{if eq .status \\\"True\\\"}}{{if not \\\$unschedulable}}{{\\\$name}}{{\\\"\\\\n\\\"}}{{end}}{{end}}{{end}}{{end}}{{end}}\"")
 ES_DATA_REPLICAS=$(echo "$NODES" | wc -l)
 
 if [ "$ES_DATA_REPLICAS" -lt 3 ]; then
